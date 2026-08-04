13502f5670b1d176cb10fa0c3fe91d194fe3a6e2
GIoriousPurpose <vvipia@naver.com>
2026-08-04 09:24:02 +0900

    Initial commit

---
+++ src/main/java/com/back/jumptospring/JumpToSpringApplication.java
@@ -0,0 +1,13 @@
+package com.back.jumptospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumpToSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JumpToSpringApplication.class, args);
+    }
+
+}
